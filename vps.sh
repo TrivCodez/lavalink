c6a462f2e7c16cef7ba08fd7da12cd6128f64f3d
TrivCodez <mopifyzai@gmail.com>
2024-08-27 14:18:06 +1000

    Create vps.sh

---
+++ vps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+# Install Java (OpenJDK)
+sudo apt install default-jdk
+
+# Install Node.js and npm
+sudo apt install nodejs
+
+# Install PM2
+sudo npm install -g pm2
+
