725d144e84f80aa05a732c57ef895f5a4a7d45d3
TrivCodez <mopifyzai@gmail.com>
2025-05-03 10:29:48 +1000

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Update the package list
-sudo apk update
+apk update
 # Install Java (OpenJDK)
-sudo apk add openjdk8
+apk add openjdk8
 # Install Node.js and npm
-sudo apk add nodejs
+apk add nodejs
 # Install PM2
-sudo npm install -g pm2
+npm install -g pm2
